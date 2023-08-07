cae33b5a5d1a09b551e8fcf7a2d356d1afc22a9a
Sarang0711 <sarangs_c20026@students.isquareit.edu.in>
2023-08-07 14:42:39 +0530

    Added Right rotation

--- RotateArray.cpp
+++ RotateArray.cpp
@@ -1,3 +1,4 @@
+// Rotate the array left K times. Accept K value from the user.
 #include<iostream>
 using namespace std;
 
@@ -18,7 +19,25 @@ void rotateLeft(int arr[],int n, int k) {
         }
 
     }
-}   
+}
+
+// Rotate the array right K times, Accept K value from the user
+
+void rotateRight(int arr[],int n, int k) {
+    k  = k % n;
+    int temp[n];
+    for(int i=0; i<n; i++) {
+        if(i < k) {
+            temp[i] = arr[i + n - k];
+        }
+        if(i >= k) {
+            temp[i] = arr[i - k];
+        }
+    }
+    for(int i=0; i<n; i++) {
+        arr[i] = temp[i];
+    }
+}
 
 int main() {
     int arr[7] = {15, 11, 7, 9, 18, 4, 1};
@@ -30,6 +49,18 @@ int main() {
     cout<<"Array after left rotation\n";
     for(int i=0; i<size; i++) {
         cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    int arr1[7] = {5, 2, 8, 1, 9, 7, 6};
+    int size1 = sizeof(arr1)/sizeof(int);
+    int k1;
+    cout<<"Enter the number of times an array should be right rotated \n";
+    cin>>k1;
+    rotateRight(arr1, size1, k1);
+    cout<<"Array after right rotation\n";
+    for(int i=0; i<size1; i++) {
+        cout<<arr1[i]<<" ";
     }
     return 0;
 }
