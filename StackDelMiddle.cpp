39432d6d8f8472b14dc061730831c7514bd7a966
Sarang0711 <sarangs_c20026@students.isquareit.edu.in>
2023-12-15 00:23:32 +0530

    Delete Middle Element from Stack

--- StackDelMiddle.cpp
+++ StackDelMiddle.cpp
@@ -4,7 +4,42 @@
 #include<stack>
 using namespace std;
 
+
+void delMiddle(stack<int> stk, int curr, int size, int count) {
+    if(stk.empty()) {
+        return;
+    }
+    if(size == 1) {
+        stk.pop();
+        return;
+    }
+    if(!stk.empty()) {
+        int top = stk.top();
+        stk.pop();
+        delMiddle(stk, curr+1, size, count+1);
+
+        if(count != size/2) {
+            stk.push(top);
+            cout<<top<<endl;
+        }
+    }
+}
+
+
+
 int main() {
-    
+    stack<int> stk;
+    stk.push(10);
+    stk.push(20);
+    stk.push(30);
+    stk.push(40);
+    stk.push(50);
+    stk.push(60);
+    stk.push(70);
+
+    int count = 0;
+    int curr = 0;
+    int size = stk.size();
+    delMiddle(stk, curr, size, count);
     return 0;
 }
