50a885c707623068130e017cdc607e72e11a9c7e
Sarang0711 <sarangs_c20026@students.isquareit.edu.in>
2023-12-20 14:59:05 +0530

    RomanToInteger

---
+++ RomanToInteger.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+
+    int romanToInt(string s) {
+        unordered_map <char, int> m;
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        int ans = 0;
+        for(int i=0; i<s.length(); i++) {
+            if (i+1 < s.length() && m[s[i]] < m[s[i+1]]) {
+                ans -= m[s[i]];
+            }
+            else {
+                ans += m[s[i]];
+            }
+        }
+        return ans;
+    }
+
+
+int main() {
+    string ss = "XVI";
+    romanToInt(ss);
+    return 0;
+}
