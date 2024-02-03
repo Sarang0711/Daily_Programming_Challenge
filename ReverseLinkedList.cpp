9f5e7d07c3c4f98e1798d580efe176f5c64564e1
Sarang0711 <sarangs_c20026@students.isquareit.edu.in>
2024-02-03 23:47:59 +0530

    Recursion

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -37,3 +37,57 @@ int main() {
   ll.create(24);
   return 0;
 }
+
+
+// Leetcode
+
+
+//  * Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+
+ //* Recursion
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode *newHead = reverseList(head->next);
+        ListNode *front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+
+    }
+};
+
+
+//* Iterative
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *curr = head;
+        ListNode *prev = NULL;
+        ListNode *tempnext;
+        while(curr != NULL) {
+            tempnext = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tempnext;
+        }
+      
+        return prev;
+
+    }
+};
+
