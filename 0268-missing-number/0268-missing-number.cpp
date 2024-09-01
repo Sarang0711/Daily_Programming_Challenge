4c7e63e87b5c265b685cd7f9b639acf4462483ed
Sarang Shelke <96944748+Sarang0711@users.noreply.github.com>
2024-09-01 22:14:08 +0530

    Time: 11 ms (81.2%), Space: 20.7 MB (70.6%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,15 +2,25 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int, int>umap;
+        int totalSum = n*(n+1)/2;
+        int sum = 0;
         for(int i=0; i<n; i++) {
-            umap[nums[i]]++;
+            sum += nums[i];
         }
-        for(int i=0; i<=n; i++) {
-            if(umap.find(i) == umap.end()) {
-                return i;
-            }
-        }
-        return -1;
+        return totalSum - sum;
+
+
+
+
+        // unordered_map<int, int>umap;
+        // for(int i=0; i<n; i++) {
+        //     umap[nums[i]]++;
+        // }
+        // for(int i=0; i<=n; i++) {
+        //     if(umap.find(i) == umap.end()) {
+        //         return i;
+        //     }
+        // }
+        // return -1;
     }
 };
